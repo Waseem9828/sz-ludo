8e6a5e7215132ba48b0f1859dea534fb73e6f062
Waseem Akram <khalilah64780@gmail.com>
2025-08-02 20:50:45 +0000

    Ongoing battle vs image ko hatakar is naye image ko add kar dijiye Jiska

--- src/components/play/battle-list.tsx
+++ src/components/play/battle-list.tsx
@@ -51,7 +51,7 @@ export default function BattleList() {
             </div>
 
             <div className="text-center">
-              <Image src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj87ucMyvvhf-gat6BSvABT1AQ0Jgo9VeN51TwF_iTZZk8j-PSlq2QTluQo7z1h70sI8CTAsPwe-UUfTiZG78qE4PcEPdm4ToEi1Q7Ei39Xo3TBYZbck2-xgEC5G7k2OWGOJQ22I3LQ82fHFILKGYEL9yP3ODdU-G_2Ho9TUOTGFEX6Xr8kFHNAnWEMy9c/s3264/74483.png" alt="vs" width={64} height={32} className="mx-auto" data-ai-hint="versus icon" />
+              <Image src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEilS2_YhPAJBDjdcIRsoMJLTWafsJuIyola3KN50zXQAZYWSSIbhLhWhOJGMG6UYkUB5ZOiVKgsy2bVstr2af0LVf2g-eWjXHnGO4Z0IbaePP4E7TSDB9x_eK8OqTidX968zc5Wn9p6uGlkLoD9iglU3KZ28_2IbXgl29zHTZgwxzMWPvbN6zhA5AhyH7s/s1600/74920.png" alt="vs" width={64} height={32} className="mx-auto" data-ai-hint="versus icon" />
               <p className="font-bold text-red-600 mt-1">₹{battle.amount}</p>
             </div>
 
