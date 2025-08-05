d329acee4cab2e33181fd3037c9fdef92a973da7
Waseem Akram <khalilah64780@gmail.com>
2025-08-05 12:18:53 +0000

    1 of 1 error
    Unhandled Runtime Error
    FirebaseError: Failed to get docum

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,7 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,8 +23,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Use initializeFirestore for more configuration options
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
+
 const auth = getAuth(app);
-const db = getFirestore(app);
 const storage = getStorage(app);
 const googleAuthProvider = new GoogleAuthProvider();
 
