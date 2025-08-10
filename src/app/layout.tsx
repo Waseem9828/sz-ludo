3f488ac6d2fb1c2d0526bfa8264270df9407b36b
Waseem Akram <khalilah64780@gmail.com>
2025-08-10 03:16:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,19 +40,6 @@ export default function RootLayout({
               <Toaster />
             </ThemeProvider>
         </AuthProvider>
-        <Script id="service-worker-registration">
-          {`
-            if ('serviceWorker' in navigator) {
-              window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js').then(registration => {
-                  console.log('Service Worker registered with scope:', registration.scope);
-                }).catch(error => {
-                  console.log('Service Worker registration failed:', error);
-                });
-              });
-            }
-          `}
-        </Script>
       </body>
     </html>
   );
